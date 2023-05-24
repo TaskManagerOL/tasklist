e1b93ded6982ad4681dc29f4d56aa2a521954253
TaskManagerOL <1975771886@qq.com>
2023-05-24 17:23:11 +0800

    new setting

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+    transpileDependencies: true,
+  lintOnSave: false,
+  devServer: {
+    open: true,
+    host: "localhost",
+    port: 8080,
+    proxy: {
+      '/api': {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api':''
+        }
+      }
+    }
+  }
 })
+
+ 
